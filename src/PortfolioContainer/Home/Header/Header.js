796e87f52501584cf0ef57dcc493187640d046f6
Yannick.C <girofare@hotmail.com>
2022-05-11 15:06:21 +0200

    add header

--- src/PortfolioContainer/Home/Header/Header.js
+++ src/PortfolioContainer/Home/Header/Header.js
@@ -1,5 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  TOTAL_SCREENS,
+  GET_SCREEN_INDEX,
+} from "../../../Utilities/CommonUtils";
+import ScrollService from "../../../Utilities/ScrollService";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./Header.css";
+import { switchScan } from "rxjs";
 
 export default function Header() {
-  return <div>Header</div>;
+  const [selectedScreen, setSelectedScreen] = useState(0);
+  const [showHeaderOptions, setShowHeaderOptions] = useState(false);
+
+  const updateCurrentScreen = (currentScreen) => {
+    if (!currentScreen || !currentScreen.screen.screenInView) return;
+    let screenIndex = GET_SCREEN_INDEX(currentScreen.ScreenInView);
+    if (screenIndex < 0) return;
+  };
+  let currentScreenSubscription =
+    ScrollService.currentScreenBroadCaster.subscribe(updateCurrentScreen);
+  const getHeaderOptions = () => {
+    return TOTAL_SCREENS.map((screen, i) => (
+      <div
+        key={screen.screen_name}
+        className={getHeaderOptionsClass(i)}
+        onClick={() => switchScreen(i, screen)}
+      >
+        <span>{screen.screen_name}</span>
+      </div>
+    ));
+  };
+
+  const getHeaderOptionsClass = (index) => {
+    let classes = "header-option";
+    if (index < TOTAL_SCREENS.length - 1) classes += "header-option-seperator";
+
+    //color of selected option
+    if (selectedScreen === index) classes += "selected-header-option";
+    return;
+  };
+
+  const switchScreen = (index, screen) => {
+    let screenComponent = document.getElementById(screen.screen_name);
+    if (!screenComponent) return;
+
+    screenComponent.scrollIntoView({ behavior: "smooth" });
+    setSelectedScreen(index);
+    setShowHeaderOptions(false);
+  };
+
+  return (
+    <div>
+      <div
+        className="header-option"
+        onClick={() => setShowHeaderOptions(!showHeaderOptions)}
+      >
+        <div className="header-parent">
+          <div
+            className="header-hamburger"
+            onClick={() => setShowHeaderOptions(!showHeaderOptions)}
+          >
+            {" "}
+            <FontAwesomeIcon
+              className="header-hamburger-bars"
+              icon={faBars}
+            />{" "}
+          </div>
+        </div>
+        <div className="header-logo">
+          <span>YANNCODE</span>
+        </div>
+        <div
+          className={
+            showHeaderOptions
+              ? "header-options show-hamburger-options"
+              : "header-options"
+          }
+        >
+          {getHeaderOptions()}
+        </div>
+      </div>
+    </div>
+  );
 }
